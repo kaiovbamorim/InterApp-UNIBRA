f30f855ee48cd5ff52d885016a9d8e2b86d1fec5
Kaio Amorim <kaiovbamorim@gmail.com>
2023-04-09 18:28:14 -0300

    Page Recovery Password

---
+++ src/app/new-password/new-password.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-new-password',
+  templateUrl: './new-password.page.html',
+  styleUrls: ['./new-password.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class NewPasswordPage implements OnInit {
+
+  constructor(private route: Router) { }
+
+  ngOnInit() {
+  }
+  backRegister(){
+    this.route.navigateByUrl('/register')
+  }
+}
