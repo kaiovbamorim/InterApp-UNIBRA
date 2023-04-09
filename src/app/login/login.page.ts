5afc5b3a7406ebd51ef46c8a005b5c27726ba034
Kaio Amorim <kaiovbamorim@gmail.com>
2023-04-09 18:27:16 -0300

    Page Login

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class LoginPage implements OnInit {
+
+  email: string = '';
+  senha: string = '';
+
+
+  passwordType: string = 'password';
+  passwordShown: boolean = false;
+
+  constructor(private toastController: ToastController, private route: Router) { }
+
+  ngOnInit() {
+  }
+
+  public togglePassword(){
+    if(this.passwordShown){
+      this.passwordShown = false;
+      this.passwordType = 'password';
+    } else {
+      this.passwordShown = true;
+      this.passwordType = "text"
+    }
+  }
+
+  login(){
+    if(this.email === 'admin@admin.com' && this.senha === 'admin'){
+      this.route.navigateByUrl('/tabs/tab1');
+      this.presentToast('bottom', 'Seja Bem Vindo!', 'success')
+    } else {
+      this.presentToast('bottom', 'ERRO, E-mail ou senha incorretos!', 'danger')
+    }
+  }
+
+  register(){
+    this.route.navigateByUrl('/register')
+  }
+  newPassword(){
+    this.route.navigateByUrl('/new-password')
+  }
+
+  async presentToast(position: 'top' | 'middle' | 'bottom', texto: string, cor: string) {
+    const toast = await this.toastController.create({
+      message: texto,
+      color: cor,
+      duration: 1500,
+      position: position
+    });
+
+    await toast.present();
+  }
+
+}
