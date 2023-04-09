7f066d33fbcd6cdfa1497ab0ffa126f274549a2b
Kaio Amorim <kaiovbamorim@gmail.com>
2023-04-09 18:27:32 -0300

    Page Register

---
+++ src/app/register/register.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Route, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class RegisterPage implements OnInit {
+
+  passwordType: string = 'password';
+  passwordShown: boolean = false;
+
+  constructor(private route: Router) { }
+
+  ngOnInit() {
+  }
+
+  public togglePassword() {
+    if (this.passwordShown) {
+      this.passwordShown = false;
+      this.passwordType = 'password';
+    } else {
+      this.passwordShown = true;
+      this.passwordType = "text"
+    }
+  }
+
+  backLogin() {
+    this.route.navigateByUrl('/login');
+  }
+
+}
