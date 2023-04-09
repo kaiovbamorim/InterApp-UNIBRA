3ce7bcd06be63944c04a4724cf0087918884b713
Kaio Amorim <kaiovbamorim@gmail.com>
2023-04-09 18:29:48 -0300

    Rotas App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,33 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    redirectTo: 'register',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    redirectTo: 'new-password',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'new-password',
+    loadComponent: () => import('./new-password/new-password.page').then( m => m.NewPasswordPage)
   },
 ];
