691d399cc4bf6d3f8529deb4efa2c98879f32d47
Kaio Amorim <kaiovbamorim@gmail.com>
2023-04-24 15:08:58 -0300

    marker

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,42 +2,59 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { GoogleMap } from '@capacitor/google-maps';
 import { url } from 'inspector';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 declare var google: any;
 
-const dados = require('./assets/database.json');
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule,HttpClientModule],
 })
 export class Tab1Page {
 
-  constructor() {}
+
+
+  constructor(
+    public http: HttpClient
+  ) {
+
+
+  }
 
   @ViewChild('map')
   mapElement!: ElementRef;
   map!: any;
+  data:any;
 
-  ngAfterViewInit() {
-    console.log(dados);
 
+  ngOnInit() {
+    this.http.get("./assets/database.json").subscribe(res =>{
+    this.data = res;
+
+    console.log(this.data)
+
+    const registro = this.data.records[0];
+const bairro = registro[this.data.fields.findIndex(f => f.id === 'bairro')];
+console.log(bairro);
     const pontosDeColeta = [
       {
         position: new google.maps.LatLng(-8.079229, -34.904414),
         icon: "./assets/marker.png",
-        title:  '<div id="content">' +
-        '<div id="siteNotice">' +
-        "</div>" +
-        '<h1 id="firstHeading" class="firstHeading">Title</h1>' +
-        '<div id="bodyContent">' +
-        "<p></p>" +
-        "</div>" +
-        "</div>"
+        title:`
+        <div><div><strong>_id</strong>${this.data.records.id}</div>
+        <div><strong>tiporesiduo</strong>: Materiais Recicláveis</div><div><strong>bairro</strong>: Soledade</div>
+        <div><strong>endereco</strong>: R. João Fernandes Viêira, 160 - Soledade, Recife</div>
+        <div><strong>complemento</strong>: </div>
+        <div><strong>observacao</strong>: PEV</div>
+        <div><strong>latitude</strong>: -8.054543</div>
+        <div><strong>longitude</strong>: -34.89133</div></div>`
       },
       {
         position: new google.maps.LatLng(-8.066962, -34.885139),
@@ -45,10 +62,16 @@ export class Tab1Page {
         title:  '<div id="content">' +
         '<div id="siteNotice">' +
         "</div>" +
-        '<h1 id="firstHeading" class="firstHeading">Title</h1>' +
         '<div id="bodyContent">' +
-        "<p></p>" +
-        "</div>" +
+        "<p>id:78 <br>" +
+        "tiporesiduo: metal, isopor, papel, plastico, vidro <br>" +
+        "bairro: Santo Antônio <br>" +
+        "endereço: Avenida Dantas Barreto <br>" +
+        "complemento: Calçada da Praça da Independência <br>" +
+        "observação: Ecoponto <br>" +
+        "latitude: -8 066962 <br>" +
+        "longitude: -34.885139 <br>" +
+        "</p>" +
         "</div>"
       },
       {
@@ -57,10 +80,16 @@ export class Tab1Page {
         title:  '<div id="content">' +
         '<div id="siteNotice">' +
         "</div>" +
-        '<h1 id="firstHeading" class="firstHeading">Title</h1>' +
         '<div id="bodyContent">' +
-        "<p></p>" +
-        "</div>" +
+        "<p>id:78 <br>" +
+        "tiporesiduo: metal, isopor, papel, plastico, vidro <br>" +
+        "bairro: Santo Antônio <br>" +
+        "endereço: Avenida Dantas Barreto <br>" +
+        "complemento: Calçada da Praça da Independência <br>" +
+        "observação: Ecoponto <br>" +
+        "latitude: -8.0540768 <br>" +
+        "longitude: -34.8936058 <br>" +
+        "</p>" +
         "</div>"
       },
       {
@@ -69,12 +98,18 @@ export class Tab1Page {
         title:  '<div id="content">' +
         '<div id="siteNotice">' +
         "</div>" +
-        '<h1 id="firstHeading" class="firstHeading">Title</h1>' +
         '<div id="bodyContent">' +
-        "<p></p>" +
-        "</div>" +
+        "<p>id:78 <br>" +
+        "tiporesiduo: metal, isopor, papel, plastico, vidro <br>" +
+        "bairro: Santo Antônio <br>" +
+        "endereço: Avenida Dantas Barreto <br>" +
+        "complemento: Calçada da Praça da Independência <br>" +
+        "observação: Ecoponto <br>" +
+        "latitude: -8.0585985 <br>" +
+        "longitude: -34.8789275 <br>" +
+        "</p>" +
         "</div>"
-      }
+      },
     ]
 
 
@@ -111,7 +146,7 @@ export class Tab1Page {
 
     }
 
-
-  }
+  })
+}
 
 }
